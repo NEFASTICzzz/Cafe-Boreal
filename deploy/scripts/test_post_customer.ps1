0b903f7b86cc81d1d5e594eed42423d44fc72513
NEFASTICzzz <nefasticzzzl@gmail.com>
2026-08-13 16:05:36 -0600

    update

--- deploy/scripts/test_post_customer.ps1
+++
@@ -1,9 +0,0 @@
-[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
-$bodyObj = @{
-    name = "Laura Araya Gamboa"
-    email = "laraya@empresa.cr"
-    identity_number = "501230456"
-}
-$bodyJson = $bodyObj | ConvertTo-Json
-$res = Invoke-RestMethod -Uri "https://127.0.0.1:8443/api/customers" -Method Post -Body $bodyJson -ContentType "application/json"
-$res | ConvertTo-Json
